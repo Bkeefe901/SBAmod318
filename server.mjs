f433ba99649e9ee997c7a11cf1416bd4a7bf07c2
Bkeefe901 <bradyjokeefe@gmail.com>
2025-09-08 11:16:34 -0400

    Created Basic Server

--- server.mjs
+++ server.mjs
@@ -1,7 +1,9 @@
 // Imports
-
+import express from 'express';
 
 // Env Setup
+const app = express();
+const PORT = 3000;
 
 
 
@@ -14,8 +16,13 @@
 
 
 // Global Error Handling Middleware
-
+app.use(function(err, req, res, next){
+    res.status(500).send(err.message);
+})
 
 
 
 // Server Listener
+app.listen(PORT, ()=>{
+    console.log(`Listening on port: ${PORT}`);
+})
